80d14610903b9c20f8edafc0a7a306bdbcb8ec01
Darin Livdahl <867781+darinlivdahl@users.noreply.github.com>
2025-02-09 04:32:52 -0600

    Add header icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,12 @@ import Heading from "../Heading/Heading";
 import Button from "../Button/Button";
 import "../../styles/global.css";
 import styles from "./Header.module.css";
-import bgImg from "./half-tone-pattern.jpg";
+// import bgImg from "./half-tone-pattern.jpg";
 
 function Header(props) {
 
     const { searchNav, headingText, headingSubText } = props;
-
+        
     function handleNewSearchClick() {
         props.onNewSearch();
     }
@@ -20,16 +20,38 @@ function Header(props) {
     }
 
     return (
-        <header className={styles.headerContainer} style={{ '--bg-img': `url(${bgImg})` }}>
-            <Heading density="compact" level="1" text={headingText} subtext={headingSubText} />
-            <nav className={styles.navContainer}>
-                {/* TODO: Display storage item count */}
-                {searchNav && <Button variant="primary" name="newSearch" onClick={handleNewSearchClick} text="New Search" />}
-                <Button variant="secondary" name="storedCreators" onClick={handleStoredClick} text="Stored Creators" />
-                <Button variant="secondary" name="storedComics" onClick={handleStoredClick} text="Stored Comics" />
-            </nav>
-        </header>
-    )
+      <header className={styles.headerContainer}>
+        <Heading
+          density="compact"
+          level="1"
+          text={headingText}
+          subtext={headingSubText}
+        />
+        <nav className={styles.navContainer}>
+          {/* TODO: Display storage item count */}
+          {searchNav && (
+            <Button
+              variant="primary"
+              name="newSearch"
+              onClick={handleNewSearchClick}
+              text="New Search"
+            />
+          )}
+          <Button
+            variant="secondary"
+            name="storedCreators"
+            onClick={handleStoredClick}
+            text="Stored Creators"
+          />
+          <Button
+            variant="secondary"
+            name="storedComics"
+            onClick={handleStoredClick}
+            text="Stored Comics"
+          />
+        </nav>
+      </header>
+    );
 }
 
 export default Header;
--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -5,7 +5,7 @@ import "../../styles/global.css";
 function Heading(props) {
     return (
         <div style={props.style} className={(props.density === "compact") ? styles.headingContainerCompact : styles.headingContainer}>
-            {props.level == 1 && <h1 className={styles.h1}>{props.text}</h1>}
+            {props.level == 1 && <h1 className={styles.h1}><i className="fa-solid fa-comment"></i> &nbsp;{props.text}</h1>}
             {props.level == 2 && <h2 className={styles.h2}>{props.text}</h2>}
             {props.level == 3 && <h3 className={styles.h3}>{props.text}</h3>}
             {props.subtext && <p className={styles.headingSubText}>{props.subtext}</p>}
